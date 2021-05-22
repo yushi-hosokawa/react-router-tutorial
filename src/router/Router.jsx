66b75277c38e4fac1f445129f6b563b6a01378c4
yushi hosokawa <49804320+yushi-hosokawa@users.noreply.github.com>
2021-05-22 17:48:03 +0900

    ルーティングを別ファイルに分ける

---
+++ src/router/Router.jsx
@@ -0,0 +1,49 @@
+import { Switch, Route } from "react-router-dom";
+import { Home } from "../Home";
+import { Page1 } from "../Page1";
+import { Page2 } from "../Page2";
+import { Page1DetailA } from "../Page1DetailA";
+import { Page1DetailB } from "../Page1DetailB";
+import { page1Routes } from "./Page1Routes";
+
+export const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route
+        path="/page1"
+        render={({ match: { url } }) => {
+          return (
+            <Switch>
+              <Route exact path={url}>
+                {page1Routes.map((route) => {
+                  console.log(`${url}${route.path}`);
+                  return (
+                    <Route
+                      key={route.path}
+                      exact={route.exact}
+                      path={`${url}${route.path}`}
+                    >
+                      {route.children}
+                    </Route>
+                  );
+                })}
+              </Route>
+              <Route path={`${url}/detailA`}>
+                <Page1DetailA />
+              </Route>
+              <Route path={`${url}/detailB`}>
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          );
+        }}
+      />
+      <Route path="/page2">
+        <Page2 />
+      </Route>
+    </Switch>
+  );
+};
