159b740f36dc0b6760a1b2699bab1e1589230bbb
yushi hosokawa <49804320+yushi-hosokawa@users.noreply.github.com>
2021-05-22 17:04:58 +0900

    ネストされたルーティングを設定

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
 import "./styles.css";
 
@@ -14,6 +16,7 @@ export default function App() {
         <Link to="/page1">page1</Link>
         <br />
         <Link to="/page2">page2</Link>
+
         <br />
       </div>
 
@@ -21,9 +24,25 @@ export default function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/page1">
-          <Page1 />
-        </Route>
+        <Route
+          path="/page1"
+          render={({ match: { url } }) => {
+            console.log(url);
+            return (
+              <Switch>
+                <Route exact path={url}>
+                  <Page1 />
+                </Route>
+                <Route path={`${url}/detailA`}>
+                  <Page1DetailA />
+                </Route>
+                <Route path={`${url}/detailB`}>
+                  <Page1DetailB />
+                </Route>
+              </Switch>
+            );
+          }}
+        />
         <Route path="/page2">
           <Page2 />
         </Route>
